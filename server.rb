81821f3e34e4910e782dc6156b99e8f249bd2248
Amber Feng <amber.feng@gmail.com>
2016-05-27 20:45:43 -0700

    Start adding database functionality.

--- server.rb
+++ server.rb
@@ -1,4 +1,13 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+
+set :database, {adapter: 'sqlite3', database: 'daily-habits.sqlite3'}
+
+class Habit < ActiveRecord::Base
+end
+
+class HabitLog < ActiveRecord::Base
+end
 
 get '/hi' do
   erb :index
